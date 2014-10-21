4e2a961a5d37fbf076f6ecc69d96cba7510fd42e
pron <ron.pressler@gmail.com>
2014-10-22 00:56:44 +0300

    initial commit

---
+++ src/main/java/OsvCapsule.java
@@ -0,0 +1,18 @@
+import java.nio.file.Path;
+
+/*
+ * Copyright (c) 2014, Parallel Universe Software Co. and Contributors. All rights reserved.
+ * 
+ * This program and the accompanying materials are licensed under the terms 
+ * of the Eclipse Public License v1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ *
+ * @author pron
+ */
+public class OsvCapsule extends Capsule {
+    public OsvCapsule(Path jarFile, Path cacheDir) {
+        super(jarFile, cacheDir);
+    }
+}
