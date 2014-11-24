8eb29a0ca0fba63826e80a575d6696f5f5f5f825
pron <ron.pressler@gmail.com>
2014-11-24 15:17:38 +0200

    license

--- src/main/java/OsvCapsule.java
+++ src/main/java/OsvCapsule.java
@@ -12,13 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-/*
- * Copyright (c) 2014, Parallel Universe Software Co. and Contributors. All rights reserved.
- * 
- * This program and the accompanying materials are licensed under the terms 
- * of the Eclipse Public License v1.0, available at
- * http://www.eclipse.org/legal/epl-v10.html
- */
 /**
  *
  * @author pron
