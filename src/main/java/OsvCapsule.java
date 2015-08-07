14dd3a95b7ddff949c285c996738fdb7ec9e1fa5
circlespainter <fabio@paralleluniverse.co>
2015-08-07 09:48:28 +0300

    :arrow_up: Port to Capsule 1.0-rc2-SNAPSHOT

--- src/main/java/OsvCapsule.java
+++ src/main/java/OsvCapsule.java
@@ -1,85 +1,113 @@
+/*
+ * Capsule
+ * Copyright (c) 2014-2015, Parallel Universe Software Co. and Contributors. All rights reserved.
+ *
+ * This program and the accompanying materials are licensed under the terms
+ * of the Eclipse Public License v1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.lang.reflect.AccessibleObject;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.nio.file.*;
+import java.nio.file.attribute.FileTime;
+import java.util.*;
+import java.util.function.Predicate;
 
 /**
  *
  * @author pron
  */
 public class OsvCapsule extends Capsule {
+
     private static final String CONF_FILE = "Capstanfile";
 
-    private static final String PROP_FILE_SEPARATOR = "file.separator";
-    private static final String PROP_BUILD_IMAGE = "capsule.image";
+    private static final String PROP_JAVA_VERSION = "java.version";
+    private static final String PROP_JAVA_HOME = "java.home";
+
     private static final String PROP_HYPERVISOR = "capsule.osv.hypervisor";
 
-    private static final String ATTR_JAVA_VERSION = "Java-Version";
+    private static final Path PATH_ROOT = Paths.get(File.separator);
+    private static final Path PATH_APP = PATH_ROOT.resolve("capsule").resolve("app");
+    private static final Path PATH_DEP = PATH_ROOT.resolve("capsule").resolve("dep");
+    private static final Path PATH_WRAPPER = PATH_ROOT.resolve("capsule").resolve("wrapper");
 
-    private static final String PATH_ROOT = "/";
-    private static final String PATH_APP = PATH_ROOT + "app";
-    private static final String PATH_DEP = PATH_ROOT + "dep";
+    private static final Map.Entry<String, Boolean> PROP_BUILD_IMAGE = ATTRIBUTE("OSV-Image-Only", T_BOOL(), false, true, "Builds an image without launching the app.");
 
-    private static final String FILE_SEPARATOR = System.getProperty(PROP_FILE_SEPARATOR);
+    private static Path hostAbsoluteOwnJarFile;
 
-    private final Path localRepo;
     private final Set<Path> deps = new HashSet<>();
-    private Path confDir;
-
-    static {
-        registerOption(PROP_BUILD_IMAGE, null, "false", "Builds an image without launching the app.");
-    }
+    private Path localRepo;
 
     public OsvCapsule(Capsule pred) {
         super(pred);
-        this.localRepo = getLocalRepo();
     }
 
+    /**
+     * Resolve relative to the container
+     */
     @Override
-    protected boolean needsAppCache() {
-        return true;
+    protected Map.Entry<String, Path> chooseJavaHome() {
+        Map.Entry<String, Path> res = super.chooseJavaHome();
+        if (res == null)
+            res = entry(getProperty(PROP_JAVA_VERSION), Paths.get(getProperty(PROP_JAVA_HOME)));
+        return entry(res.getKey(), Paths.get(File.separator));
     }
 
+    private static <K, V> Map.Entry<K, V> entry(K k, V v) {
+        return new AbstractMap.SimpleImmutableEntry<>(k, v);
+    }
+
+    @SuppressWarnings("deprecation")
     @Override
-    protected String processOutgoingPath(Path p) {
-        if (p == null)
-            return null;
-        p = p.normalize().toAbsolutePath();
-        if (p.startsWith(localRepo))
-            deps.add(p);
-        return move(p);
+    protected List<Path> resolve0(Object x) {
+        if (x instanceof Path && ((Path) x).isAbsolute()) {
+            Path p = (Path) x;
+            if (localRepo != null && p.startsWith(localRepo))
+                deps.add(p);
+            p = move(p);
+            return super.resolve0(p);
+        }
+        return super.resolve0(x);
     }
 
     @Override
     protected final Path getJavaExecutable() {
-        return Paths.get("java.so");
+        return Paths.get("/java.so");
     }
 
     @Override
-    protected ProcessBuilder prelaunch(List<String> args) {
-        final boolean build = Boolean.parseBoolean(System.getProperty(PROP_BUILD_IMAGE));
+    protected final ProcessBuilder prelaunch(List<String> jvmArgs, List<String> args) {
         try {
-            // Use the original ProcessBuilder to create the Capstanfile
-            final ProcessBuilder pb = super.prelaunch(args);
-            this.confDir = getAppCache();
-            writeCapstanfile(confDir.resolve(CONF_FILE), pb);
+            this.localRepo = getLocalRepo();
 
-            log(LOG_VERBOSE, "Capstanfile: " + confDir.resolve(CONF_FILE));
+            final List<String> newJvmArgs = new ArrayList<>();
+            newJvmArgs.addAll(jvmArgs);
+            newJvmArgs.remove("-server");
+            newJvmArgs.remove("-client");
+
+            // Use the original ProcessBuilder to create the Capstanfile
+            final ProcessBuilder pb = super.prelaunch(newJvmArgs, args);
+
+            final boolean build = getAttribute(PROP_BUILD_IMAGE);
+
+            if (isBuildNeeded() && !build)
+                buildImage();
+
+            writeCapstanfile(pb);
 
             // ... and create a new ProcessBuilder to launch capstan
             final ProcessBuilder pb1 = new ProcessBuilder();
-            pb1.directory(confDir.toFile());
+            pb1.directory(getConfDir().toFile());
 
             pb1.command().add("capstan");
+
             pb1.command().add(build ? "build" : "run");
             if (System.getProperty(PROP_HYPERVISOR) != null)
                 pb1.command().addAll(Arrays.asList("-p", System.getProperty(PROP_HYPERVISOR)));
@@ -92,17 +120,52 @@ public class OsvCapsule extends Capsule {
         }
     }
 
-    @Override
-    protected Map<String, String> buildEnvironmentVariables(Map<String, String> env) {
-        return env;
+    private void buildImage() throws IOException {
+        log(LOG_VERBOSE, "Re-creating OSV image");
+
+        final ProcessBuilder pb1 = new ProcessBuilder();
+        pb1.directory(getConfDir().toFile());
+
+        pb1.command().add("capstan");
+        pb1.command().add("build");
+
+        try {
+            if (pb1.start().waitFor() != 0)
+                throw new RuntimeException("Image build failed");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        log(LOG_VERBOSE, "OSV image re-created");
     }
 
-    @Override
-    protected List<String> buildJVMArgs() {
-        final List<String> args = super.buildJVMArgs();
-        args.remove("-server");
-        args.remove("-client");
-        return args;
+    public Path getConfDir() throws IOException {
+        final Path ret = getWritableAppCache().resolve("osv");
+        if (!Files.exists(ret))
+            Files.createDirectories(ret);
+        return ret;
+    }
+
+    public Path getConfFile() throws IOException {
+        return getConfDir().resolve(CONF_FILE);
+    }
+
+    private boolean isBuildNeeded() throws IOException {
+        if (!Files.exists(getConfFile()))
+            return true;
+        try {
+            FileTime jarTime = Files.getLastModifiedTime(getJarFile());
+            if (isWrapperCapsule()) {
+                final FileTime wrapperTime = Files.getLastModifiedTime(findOwnJarFile());
+                if (wrapperTime.compareTo(jarTime) > 0)
+                    jarTime = wrapperTime;
+            }
+
+            final FileTime confTime = Files.getLastModifiedTime(getConfFile());
+            return confTime.compareTo(jarTime) < 0;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -110,20 +173,30 @@ public class OsvCapsule extends Capsule {
         return splitClassPath("/usr/java/packages/lib/amd64:/usr/lib64:/lib64:/lib:/usr/lib");
     }
 
-    private void writeCapstanfile(Path file, ProcessBuilder pb) throws IOException {
-        try (PrintWriter out = new PrintWriter(new OutputStreamWriter(Files.newOutputStream(file), Charset.defaultCharset()))) {
+    private void writeCapstanfile(ProcessBuilder pb) throws IOException {
+        try (final PrintWriter out = new PrintWriter(new OutputStreamWriter(Files.newOutputStream(getConfFile()), Charset.defaultCharset()))) {
             out.println("base: " + getBaseImage());
-
+            out.println();
+            final List<String> command = new ArrayList<>();
+            command.addAll(pb.command());
+            command.removeIf(new Predicate<String>() {
+                @Override
+                public boolean test(String s) {
+                    return "-server".equals(s) || "-client".equals(s);
+                }
+            });
+            out.println("cmdline: " + String.join(" ", command));
+            out.println();
             out.println("files:");
             out.println(file(getJarFile()));
-            if (getAppCache() != null) {
-                for (Path p : listDir(getAppCache(), "**", true))
+            if (getWritableAppCache() != null) {
+                for (Path p : listDir(getWritableAppCache(), "**", true))
                     out.println(file(p));
             }
             for (Path p : deps)
                 out.println(file(p));
 
-            out.println("cmdline: " + toStringValue(pb.command()));
+            log(LOG_VERBOSE, "Written capstanfile: " + getConfFile());
         }
     }
 
@@ -131,56 +204,96 @@ public class OsvCapsule extends Capsule {
         return "  " + move(p) + ": " + p;
     }
 
-    private String move(Path p) {
+    private Path move(Path p) {
+        if (p == null)
+            return null;
+
         p = p.normalize().toAbsolutePath();
+
+        if (p.startsWith(Paths.get("/dep")) || p.startsWith(Paths.get("/app")))
+            return p;
+
         if (p.equals(getJavaExecutable().toAbsolutePath()))
-            return PATH_ROOT + getJavaExecutable();
+            return PATH_ROOT.resolve(getJavaExecutable());
         if (p.equals(getJarFile()))
             return moveJarFile(p);
-        else if (getAppCache() != null && p.startsWith(getAppCache()))
-            return moveAppCache(p);
-        else if (p.startsWith(localRepo))
-            return moveDep(p);
+        if (p.equals(findOwnJarFile()))
+            return moveWrapperFile(p);
+        else if (getAppDir() != null && p.startsWith(getAppDir()))
+            return move(p, getAppDir(), PATH_APP);
+        else if (localRepo != null && p.startsWith(localRepo))
+            return move(p, localRepo, PATH_DEP);
         else if (getPlatformNativeLibraryPath().contains(p))
-            return toString(p);
+            return p;
+        else if (p.startsWith(getJavaHome()))
+            return p; // already moved in chooseJavaHome
         else
             throw new IllegalArgumentException("Unexpected file " + p);
     }
 
-    private String moveJarFile(Path p) {
-        return PATH_ROOT + p.getFileName();
+    private Path moveJarFile(Path p) {
+        return PATH_ROOT.resolve(p.getFileName());
     }
 
-    private String moveAppCache(Path p) {
-        return move(p, getAppCache(), PATH_APP);
-    }
-
-    private String moveDep(Path p) {
-        return PATH_DEP + "/" + p.getFileName();
-    }
-
-    private static String move(Path what, Path fromDir, String toDir) {
-        assert what.startsWith(fromDir);
-        return toDir + "/" + toString(fromDir.relativize(fromDir));
-    }
-
-    private static String toString(Path p) {
-        return isWindows() ? p.toString().replace(FILE_SEPARATOR, "/") : p.toString();
+    private Path moveWrapperFile(Path p) {
+        return PATH_WRAPPER.resolve(p.getFileName());
     }
 
     private String getBaseImage() {
-        if (hasAttribute(ATTR_JAVA_VERSION)) {
-            switch (javaVersion(getAttribute(ATTR_JAVA_VERSION))) {
+        if (hasAttribute(Capsule.ATTR_JAVA_VERSION)) {
+            switch (javaVersion(getAttribute(Capsule.ATTR_JAVA_VERSION))) {
                 case 7:
                     return "cloudius/osv-openjdk";
                 case 8:
                     return "cloudius/osv-openjdk8";
                 default:
                     // getJavaHome(); ...
-                    throw new RuntimeException("No known OSv image for Java version " + getAttribute(ATTR_JAVA_VERSION));
+                    throw new RuntimeException("No known OSv image for Java version " + getAttribute(Capsule.ATTR_JAVA_VERSION));
             }
         } else
             return "cloudius/osv-openjdk8";
+    }
+
+    // TODO Factor out this common utilities
+    //<editor-fold defaultstate="collapsed" desc="Both capsule- and container-related overrides & utils">
+    private Path getLocalRepo() {
+        final Capsule mavenCaplet = sup("MavenCapsule");
+        if (mavenCaplet == null)
+            return null;
+        try {
+            return (Path) accessible(mavenCaplet.getClass().getDeclaredMethod("getLocalRepo")).invoke(mavenCaplet);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static <T extends AccessibleObject> T accessible(T obj) {
+        if (obj == null)
+            return null;
+        obj.setAccessible(true);
+        return obj;
+    }
+
+    private static Path findOwnJarFile() {
+        if (hostAbsoluteOwnJarFile == null) {
+            final URL url = OsvCapsule.class.getClassLoader().getResource(OsvCapsule.class.getName().replace('.', '/') + ".class");
+            if (url != null) {
+                if (!"jar".equals(url.getProtocol()))
+                    throw new IllegalStateException("The Capsule class must be in a JAR file, but was loaded from: " + url);
+                final String path = url.getPath();
+                if (path == null) //  || !path.startsWith("file:")
+                    throw new IllegalStateException("The Capsule class must be in a local JAR file, but was loaded from: " + url);
+
+                try {
+                    final URI jarUri = new URI(path.substring(0, path.indexOf('!')));
+                    hostAbsoluteOwnJarFile = Paths.get(jarUri);
+                } catch (URISyntaxException e) {
+                    throw new AssertionError(e);
+                }
+            } else
+                throw new RuntimeException("Can't locate capsule's own class");
+        }
+        return hostAbsoluteOwnJarFile;
     }
 
     private static int javaVersion(String v) {
@@ -200,4 +313,73 @@ public class OsvCapsule extends Capsule {
             res.add(Paths.get(p));
         return res;
     }
+
+    protected static List<Path> listDir(Path dir, String glob, boolean regular) {
+        //noinspection Convert2Diamond
+        return listDir(dir, glob, false, regular, new ArrayList<Path>());
+    }
+
+    private static List<Path> listDir(Path dir, String glob, boolean recursive, boolean regularFile, List<Path> res) {
+        return listDir(dir, splitGlob(glob), recursive, regularFile, res);
+    }
+
+    private static List<String> splitGlob(String glob) { // splits glob pattern by directory
+        return glob != null ? Arrays.asList(glob.split("\\".equals(File.separator) ? "\\\\" : File.separator)) : null;
+    }
+
+    @SuppressWarnings({"null", "Convert2Diamond"})
+    private static List<Path> listDir(Path dir, List<String> globs, boolean recursive, boolean regularFile, List<Path> res) {
+        PathMatcher matcher = null;
+        if (globs != null) {
+            while (!globs.isEmpty() && "**".equals(globs.get(0))) {
+                recursive = true;
+                globs = globs.subList(1, globs.size());
+            }
+            if (!globs.isEmpty())
+                matcher = dir.getFileSystem().getPathMatcher("glob:" + globs.get(0));
+        }
+
+        final List<Path> ms = (matcher != null || recursive) ? new ArrayList<Path>() : res;
+        final List<Path> mds = matcher != null ? new ArrayList<Path>() : null;
+        final List<Path> rds = recursive ? new ArrayList<Path>() : null;
+
+        try (final DirectoryStream<Path> fs = Files.newDirectoryStream(dir)) {
+            for (Path f : fs) {
+                if (recursive && Files.isDirectory(f))
+                    rds.add(f);
+                if (matcher == null) {
+                    if (!regularFile || Files.isRegularFile(f))
+                        ms.add(f);
+                } else {
+                    if (matcher.matches(f.getFileName())) {
+                        if (globs.size() == 1 && (!regularFile || Files.isRegularFile(f)))
+                            ms.add(f);
+                        else if (Files.isDirectory(f))
+                            mds.add(f);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Collections.sort(ms); // sort to give same results on all platforms (hopefully)
+        if (res != ms) {
+            res.addAll(ms);
+
+            //noinspection UnusedLabel
+            recurse:
+                for (final List<Path> ds : Arrays.asList(mds, rds)) {
+                    if (ds == null)
+                        continue;
+                    Collections.sort(ds);
+                    final List<String> gls = (ds == mds ? globs.subList(1, globs.size()) : globs);
+                    for (final Path d : ds)
+                        listDir(d, gls, recursive, regularFile, res);
+                }
+        }
+
+        return res;
+    }
+    //</editor-fold>
 }
